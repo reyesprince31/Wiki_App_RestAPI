9b294d12a5ae1ea013039796a78e73d13a862cf0
Prince Reyes <krayzerprince@gmail.com>
2023-07-14 11:43:28 +0800

    Added Delete route and refactor all using app.route() and chain them

--- app.js
+++ app.js
@@ -8,39 +8,57 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get("/", async (req, res) => {
-  const result = await Article.find({});
+app
+  .route("/articles")
+  .get(async (req, res) => {
+    const result = await Article.find({});
 
-  if (result.length) {
-    // console.log(result);
-    res.send(result);
-    result.forEach((article) => {
-      console.log(article.title);
+    if (result.length) {
+      console.log(result);
+      res.send(result);
+      result.forEach((article) => {
+        console.log(article.title);
+      });
+    } else {
+      console.log("No Result Found");
+    }
+  })
+
+  .post(async (req, res) => {
+    const { title, content } = req.body;
+
+    const newArticle = new Article({
+      title: title,
+      content: content,
     });
-  } else {
-    console.log("No Result Found");
-  }
-});
 
-app.post("/articles", async (req, res) => {
-  const { title, content } = req.body;
+    try {
+      await newArticle.save();
+      res.status(201).json({
+        message: "Article created successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  })
 
-  const newArticle = new Article({
-    title: title,
-    content: content,
+  .delete(async (req, res) => {
+    const title = req.body;
+    const articlesToDelete = await Article.deleteMany({});
+
+    if (articlesToDelete.length) {
+      await articlesToDelete.deleteMany();
+      res.status(200).json({
+        message: "Articles deleted successfully",
+      });
+    } else {
+      res.status(404).json({
+        message: "No articles found with title ",
+      });
+    }
   });
-
-  try {
-    await newArticle.save();
-    res.status(201).json({
-      message: "Article created successfully",
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
 
 app.listen(port, () => {
   console.log(`Successfully Connected to Port ${port}`);
